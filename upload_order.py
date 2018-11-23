a3bc786f3f8b20509a5d99ed8e0bac8c129586bc
HArry <phuccoi996@gmail.com>
2018-11-23 16:52:36 +0700

    done upload_orders

--- upload_order.py
+++ upload_order.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
+import warnings
+warnings.filterwarnings("ignore")
+import pandas as pd
+import glob,os
+from shutil import copyfile
+import math
+import numpy as np
+masterdata =  pd.read_excel('metadata.xlsx')
+masterdata.head()
+
 def insert_upload_order(file_name):
     try:
-        masterdata  =  pd.read_excel('C:\\Users\\phuc.trinh\\Documents\\Upload order 2018-11-16\\test\\metadata.xlsx')
+        masterdata  =  pd.read_excel('metadata.xlsx')
         try:
             data = pd.read_excel(file_name)
         except:
@@ -11,9 +21,12 @@ def insert_upload_order(file_name):
         data = data.rename(columns=data.iloc[0])
         data = data[1:]
         if u'PHASE' not in data.columns:
-            q = (file_name.split("\\")[-1]," has a problem with structure")
+            os.remove(file_name)
+            q = (file_name.split("\\")[-1]," has a problem with structure, no PHASE")
             return q
-        print (data[data[u'ALBUM'].notnull() == True and data[u'HỢP ĐỒNG'].notnull() == True])
+        if u'ALBUM' in data.columns:
+            ALBUM = data[data[u'ALBUM'].notnull() == True][u'ALBUM']
+            HD = data[data[u'HỢP ĐỒNG'].notnull() == True][u'HỢP ĐỒNG']
         if u'ID VIDEO' in data.columns:
             try:
                 try:
@@ -61,69 +74,77 @@ def insert_upload_order(file_name):
             data.rename(columns={u'Video ID':u'VIDEO ID'},inplace=True)
         except KeyError:
             pass
-        if (str(data.columns[1])=='nan' and str(data.columns[2])=='nan'):
-            try:
-                data = data.drop('VIDEO ID',1)
-            except KeyError:
-                data = data.drop('Video ID',1)
-            data = data[data.iloc[:, 1].notnull()==True]
-            column_names = data.columns.values
-            column_names[1] = u'VIDEO ID'
-            data.columns = column_names
+        try:
+            if (str(data.columns[1])=='nan' and str(data.columns[2])=='nan'):
+                try:
+                    data = data.drop('VIDEO ID',1)
+                except KeyError:
+                    data = data.drop('Video ID',1)
+                data = data[data.iloc[:, 1].notnull()==True]
+                column_names = data.columns.values
+                column_names[1] = u'VIDEO ID'
+                data.columns = column_names
+        except:
+            pass
         data = data[data['VIDEO ID'].notnull()]
-        if 'ALBUM' in data.columns  and len(data) >200:
-            copyfile(file_name,"C:\\Users\\phuc.trinh\\Documents\\Upload order 2018-11-16\\test\\undone\\"+file_name.split("\\")[-1])
-            os.remove(file_name)
-            q = (file_name.split("\\")[-1],"contains Album")
-            return q
-        else:
+        try:
+            data['TÊN NỘI DUNG']
+        except KeyError:
             try:
-                data['TÊN NỘI DUNG']
+                data[u'NỘI DUNG']
+                data = data.drop(u'TÊN NỘI DUNG', 1)
+                data.rename(columns={u'NỘI DUNG':u'TÊN NỘI DUNG'},inplace=True)
             except KeyError:
                 try:
-                    data[u'NỘI DUNG']
-                    data = data.drop(u'TÊN NỘI DUNG', 1)
-                    data.rename(columns={u'NỘI DUNG':u'TÊN NỘI DUNG'},inplace=True)
+                    data[u'TÊN BÀI HÁT']
+                    data.rename(columns={u'TÊN BÀI HÁT':u'TÊN NỘI DUNG'},inplace=True)
                 except KeyError:
-                    try:
-                        data[u'TÊN BÀI HÁT']
-                        data.rename(columns={u'TÊN BÀI HÁT':u'TÊN NỘI DUNG'},inplace=True)
-                    except KeyError:
-                        pass
-                    try:
-                        data[u'Tên bài hát | Tên ca sĩ /Lyric Video']
-                        data.rename(columns={'Tên bài hát | Tên ca sĩ /Lyric Video':u'TÊN NỘI DUNG'},inplace=True)
-                    except KeyError:
-                        pass
-                    try:
-                        data[u'TÊN PHIM']
-                        data.rename(columns={u'TÊN PHIM':u'TÊN NỘI DUNG'},inplace=True)
-                    except KeyError:
-                        pass
+                    pass
+                try:
+                    data[u'Tên bài hát | Tên ca sĩ /Lyric Video']
+                    data.rename(columns={'Tên bài hát | Tên ca sĩ /Lyric Video':u'TÊN NỘI DUNG'},inplace=True)
+                except KeyError:
+                    pass
+                try:
+                    data[u'TÊN PHIM']
+                    data.rename(columns={u'TÊN PHIM':u'TÊN NỘI DUNG'},inplace=True)
+                except KeyError:
+                    pass
         try:
             data[u'Series Name']
             data.rename(columns={u'Series Name':u'SERIES NAME'},inplace=True)
         except KeyError:
             pass
-        try:
-            data[u'Series Name']
-            data.rename(columns={u'Series Name':u'Series Name'},inplace=True)
-        except KeyError:
-            pass
+
+        if u'ALBUM' in data.columns:
+            if (len(data) != len(ALBUM) or len(data) != len(HD)):
+                q = ("The length is not equal in",file_name.split("\\")[-1])
+                copyfile(file_name,"undone\\"+file_name.split("\\")[-1])
+                os.remove(file_name)
+                return q
+            else:
+                if ALBUM.str.contains("VN").any() == True:
+                    pass
+                else:
+                    data[u'TÊN NỘI DUNG'] = ALBUM.values
+                data[u'HỢP ĐỒNG'] = HD.values
         try:
             merge_data =  data[[u'VIDEO ID',u'TÊN NỘI DUNG',u'HỢP ĐỒNG',u'CATEGORY',u'SUB CATEGORY',u'SUB CATEGORY 2',u'SEASON',u'PHASE',u'SERIES NAME']]
             frames = [masterdata,merge_data]
             result = pd.concat(frames)
-            result.to_excel('C:\\Users\\phuc.trinh\\Documents\\Upload order 2018-11-16\\test\\metadata.xlsx')
+            result.to_excel('metadata.xlsx')
             q = (file_name.split("\\")[-1] , "is succeed")
         except:
-            copyfile(file_name,"C:\\Users\\phuc.trinh\\Documents\\Upload order 2018-11-16\\test\\undone\\"+file_name.split("\\")[-1])
+            copyfile(file_name,"undone\\"+file_name.split("\\")[-1])
             os.remove(file_name)
             q = (file_name.split("\\")[-1]," has a problem with merging data")
         pass
     except (IndexError) as e:
-        copyfile(file_name,"C:\\Users\\phuc.trinh\\Documents\\Upload order 2018-11-16\\test\\undone\\"+file_name.split("\\")[-1])
+        copyfile(file_name,"undone\\"+file_name.split("\\")[-1])
         os.remove(file_name)
         q = (file_name.split("\\")[-1]," has a problem with structure")
         pass
     return q
+
+for file in glob.glob("Upload orders\\*"):
+    print insert_upload_order(file)
